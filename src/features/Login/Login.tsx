581c77ec4dbc1fafba8f92846dffe3897f2ee0e5
Tenadopt <yury.ignatiev@gmail.com>
2023-11-30 18:07:03 +0300

    add habdle errors

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -9,6 +9,11 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import {useFormik} from "formik";
 
+type FormikErrorType = {
+    email?: string
+    password?: string
+}
+
 export const Login = () => {
 
     const formik = useFormik({
@@ -17,10 +22,26 @@ export const Login = () => {
             password: '',
             rememberMe: false
         },
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            if (!values.email) {
+                errors.email = 'Required';
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address';
+            }
+
+            if (!values.password) {
+                errors.password = 'Required'
+            } else if (values.password.length<5) {
+                errors.password = 'Password is too short'
+            }
+                return errors
+            },
         onSubmit: values => {
             alert(JSON.stringify(values, null, 2))
         }
     })
+
 
     console.log(formik.values)
 
@@ -37,29 +58,44 @@ export const Login = () => {
                     <p>Email: free@samuraijs.com</p>
                     <p>Password: free</p>
                 </FormLabel>
-                <FormGroup>
-                    <TextField
-                        label="Email"
-                        margin="normal"
-                        name={'email'}
-                        onChange={formik.handleChange}
-                        value={formik.values.email}/>
-                    <TextField
-                        type="password"
-                        label="Password"
-                        margin="normal"
-                        name={'password'}
-                        onChange={formik.handleChange}
-                        value={formik.values.password}
-                    />
-                    <FormControlLabel label={'Remember me'}
-                                      control={<Checkbox/>}
-                                      name={'rememberMe'}
-                                      checked={formik.values.rememberMe}/>
-                    <Button type={'submit'} variant={'contained'} color={'primary'}>
-                        Login
-                    </Button>
-                </FormGroup>
+                <form onSubmit={formik.handleSubmit}>
+                    <FormGroup>
+                        <TextField
+                            label="Email"
+                            margin="normal"
+                            name={'email'}
+                            error={!!(formik.touched.email && formik.errors.email)}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.email}
+                            helperText={formik.touched.email && formik.errors.email}
+                        />
+
+
+                        <TextField
+                            type="password"
+                            label="Password"
+                            margin="normal"
+                            name={'password'}
+                            error={!!(formik.touched.password && formik.errors.password)}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                            value={formik.values.password}
+                        />
+
+                        {formik.touched.password && formik.errors.password && <div style={{color: 'red'}}>{formik.errors.password}</div>}
+
+                        <FormControlLabel label={'Remember me'}
+                                          control={<Checkbox/>}
+                                          name={'rememberMe'}
+                                          checked={formik.values.rememberMe}
+                                          onChange={formik.handleChange}
+                        />
+                        <Button type={'submit'} variant={'contained'} color={'primary'}>
+                            Login
+                        </Button>
+                    </FormGroup>
+                </form>
             </FormControl>
         </Grid>
     </Grid>
